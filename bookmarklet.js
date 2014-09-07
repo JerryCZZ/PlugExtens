17544f0e0f76e5efa1bc41b0517fd9064da37dfe
JerryCZZ <jerryk8@seznam.cz>
2014-09-07 12:22:06 +0200

    a

--- bookmarklet.js
+++ bookmarklet.js
@@ -55,7 +55,7 @@ gui.add(settings, 'ViditelnostVidea',0,1).onChange(showHideVideo);
 gui.add(settings, 'Autowoot').onChange(setWootBehavior);
 gui.add(settings, 'VlozeneObrazky').onChange(doInlineImages);
 
-gui.add(settings,'VelikostVidea', ['Normalni','Velky']).onChange(updateVideoSize)
+gui.add(settings,'VelikostVidea', ['Normalni','large']).onChange(updateVideoSize)
 var themeSettingsObject = {}
 for(var i = 0; i < themes.length; i++) {
 	var theme = themes[i];
